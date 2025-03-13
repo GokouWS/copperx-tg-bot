a0e1c020918c6295e102d0f402919805e6fb4fec
GokouWS <lynxyyeu@gmail.com>
2025-03-13 16:49:17 +0000

    Register balance commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,10 +5,23 @@ import * as login from "./login";
 import * as send from "./send";
 import * as withdraw from "./withdraw";
 import { bot, MyContext } from "../bot";
+import { checkTokenExpiration } from "../utils/middleware";
 
 export function setupCommands() {
-  // Login command
+  // Login command - Does NOT need the middleware, as it's for authentication
   bot.command("login", login.handleLogin);
+
+  // --- Commands that require authentication (and token check) ---
+  // Use the middleware for all commands that require a valid token
+
+  // Balance commands
+  bot.command("balance", checkTokenExpiration, balance.handleBalance);
+  bot.command("default_wallet", checkTokenExpiration, balance.handleDefaultWallet);
+  bot.command(
+    "change_default_wallet",
+    checkTokenExpiration,
+    balance.handleChangeDefaultWallet,
+  );
 
   // General message handler.  This needs to come *after* specific command handlers.
   bot.on("text", (ctx: MyContext) => {
