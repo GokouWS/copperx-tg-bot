09af5384e81b2595ef4f59689f79e2e0e0160eb7
GokouWS <lynxyyeu@gmail.com>
2025-03-13 11:30:01 +0000

    Added helper function for input validation

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,1 +1,19 @@
 // Utility functions
+
+// Basic escaping function
+export function escapeInput(input: string): string {
+  return input.replace(/[<>&"]/g, function (char) {
+    switch (char) {
+      case "<":
+        return "&lt;";
+      case ">":
+        return "&gt;";
+      case "&":
+        return "&amp;";
+      case '"':
+        return "&quot;";
+      default:
+        return char;
+    }
+  });
+}
