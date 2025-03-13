2aacd2c623f39e3c04841b0d094bd5e03c47cffa
GokouWS <lynxyyeu@gmail.com>
2025-03-13 11:53:44 +0000

    Register /login commands and general text handler

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,27 @@
 // Combines all command modules
-import {Context} from "telegraf"
-import * as balance from "./balance"
-import * as login from "./login"
-import * as send from "./send"
-import * as withdraw from "./withdraw"
+import { Context } from "telegraf";
+import * as balance from "./balance";
+import * as login from "./login";
+import * as send from "./send";
+import * as withdraw from "./withdraw";
+import { bot } from "../bot";
+
+export function setupCommands() {
+  // Login command
+  bot.command("login", login.handleLogin);
+
+  // General message handler.  This needs to come *after* specific command handlers.
+  bot.on("text", (ctx) => {
+    switch (ctx.session.step) {
+      case "awaitingEmail":
+        login.handleEmailInput(ctx);
+        break;
+      case "awaitingOtp":
+        login.handleOtpInput(ctx);
+        break;
+      default:
+        // Show a default message.
+        ctx.reply("Sorry, I didn't understand that command.");
+    }
+  });
+}
