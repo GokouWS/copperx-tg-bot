61c673abf68c76d5bde42e8f39fe38bc288031bc
GokouWS <lynxyyeu@gmail.com>
2025-03-16 23:04:58 +0000

    Add pusher and session initialisation on login

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Main bot entry point
-import { bot } from "./bot";
+import { getUserProfile, isTokenExpired } from "./api";
+import { bot, clearUserSession, initializeUserSession } from "./bot";
 import { setupCommands } from "./commands";
 
 // Setup commands
@@ -15,3 +16,35 @@ bot
     console.error("Failed to start bot:", err);
     process.exit(1); // Exit with error code
   });
+
+//fetch the orgId after login and initialize user session.
+bot.command("start", async (ctx) => {
+  const tokenData = ctx.session.tokenData;
+  if (tokenData && !isTokenExpired(tokenData)) {
+    try {
+      const { token } = tokenData;
+      const userProfile = await getUserProfile(token);
+      const organizationId = userProfile.organizationId;
+      console.log("Organization ID from /start:", organizationId);
+      if (organizationId) {
+        initializeUserSession(ctx.chat.id, token, organizationId); // Initialize
+      } else {
+        ctx.reply("Could not retrieve organization ID. Please contact support.");
+      }
+    } catch (error) {
+      ctx.reply("Failed to initialize user session.");
+    }
+  } else {
+    ctx.reply("Welcome to the Copperx bot, /login to begin.");
+  }
+});
+
+// Enable graceful stop
+process.once("SIGINT", () => {
+  clearUserSession();
+  bot.stop("SIGINT");
+});
+process.once("SIGTERM", () => {
+  clearUserSession();
+  bot.stop("SIGTERM");
+});
