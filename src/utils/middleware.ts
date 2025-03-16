10412b5c039ab01fc99b18430e82b261a12cbb94
GokouWS <lynxyyeu@gmail.com>
2025-03-16 23:01:52 +0000

    Auth middleware cleanup

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -3,12 +3,14 @@ import { MyContext } from "../bot"; // Import your custom context
 import { isTokenExpired } from "../api";
 
 export const checkTokenExpiration: MiddlewareFn<MyContext> = async (ctx, next) => {
-  if (!ctx.session.tokenData) {
+  const token = ctx.session.tokenData;
+
+  if (!token) {
     // No token at all: User is not logged in
     return ctx.reply("Please log in first using /login.");
   }
 
-  if (ctx.session.tokenData && isTokenExpired(ctx.session.tokenData)) {
+  if (isTokenExpired(token)) {
     delete ctx.session.tokenData; // Clear the expired token
     return ctx.reply("Your session has expired. Please log in again using /login.");
   }
