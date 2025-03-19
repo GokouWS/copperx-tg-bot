3bcb81996002f4eba2113ba7208dceb17137686f
GokouWS <lynxyyeu@gmail.com>
2025-03-19 14:35:08 +0000

    Add UserProfile type

--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,20 @@ export type Wallet = {
 };
 
 export type WalletsResponse = Wallet[];
+
+export type UserProfile = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  profileImage: string;
+  organizationId: string;
+  role: "owner";
+  status: "pending";
+  type: "individual";
+  relayerAddress: string;
+  flags: string[];
+  walletAddress: string;
+  walletId: string;
+  walletAccountType: string;
+};
